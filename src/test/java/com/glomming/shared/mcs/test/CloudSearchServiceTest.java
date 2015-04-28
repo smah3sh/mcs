5453f8d7b2c284b5b0834aae30a7bf30a62b68af
smahesh <smahesh69@gmail.com>
2015-04-27 22:00:57 -0700

    Added sample JSON file

--- src/test/java/com/glomming/shared/mcs/test/CloudSearchServiceTest.java
+++ src/test/java/com/glomming/shared/mcs/test/CloudSearchServiceTest.java
@@ -102,13 +102,6 @@ public class CloudSearchServiceTest {
   String [] queryStrings = {queryOne, queryTwo, queryThree, queryFour};
   String [] facetStrings = {facetOne, facetTwo, facetThree};
 
-//  Facets
-//  {"weapons":{"sort":"count","size":5}}
-//  {"points":{"buckets":["[0,100]","[101,200]","[201,300]","[301,400]","[401,}"]}}
-//  {"weapons":{"sort":"count","size":5},"points":{"buckets":["[0,100]","[101,200]","[201,300]","[301,400]","[401,}"]}
-//  Query String
-// "(and (or weapons:'GUN' weapons:'CANNON' weapons:'DRONE')(and last_login:['2013-05-25T00:00:00Z','2014-10-25T00:00:00Z'])(and points:[100, 200])(and elo_rating:[1000, 2000]))";
-//
   @Test
   public void testSearch() throws Exception {
     long numResults = 10;
